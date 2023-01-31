66baa50b1d4934e7aff50505b0f2c4ee181491e7
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-01-31 11:08:42 +0200

    Added wheel collidere and updated its rotation with the wheel meshes

--- Assets/Scripts/FourWheelMovemet.cs
+++ Assets/Scripts/FourWheelMovemet.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class FourWheelMovemet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    WheelCollider[] wheelColliders;
+
+    [SerializeField]
+    Transform[] wheelMeshes;
+
+    void FixedUpdate()
     {
-        
+        for (int i = 0; i < wheelColliders.Length; i++)
+        {
+            UpdateWheelPos(wheelColliders[i], wheelMeshes[i]);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void UpdateWheelPos(WheelCollider wheelCollider, Transform wheelMeshe)
     {
-        
+        Vector3 colliderPosition = wheelCollider.transform.position;
+        Quaternion colliderRotation = wheelCollider.transform.rotation;
+
+        wheelCollider.GetWorldPose(out colliderPosition, out colliderRotation);
+
+        //wheelMeshe.transform.position = colliderPosition;
+        wheelMeshe.transform.rotation = colliderRotation;
     }
 }
